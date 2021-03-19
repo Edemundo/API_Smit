129534c3bf1dab3bf86ff54fc75deb2523450a6b
Pedro Sodre <pedssodre@gmail.com>
2021-03-19 17:05:15 -0300

    DimTipoProcuraService done!

--- Documents/Projetos/trajetoria_cidadao/src/main/java/com/smads/covs/trajetoria_cidadao/services/sisa_siscr_sisrua/DimTipoProcuraService.java
+++ Documents/Projetos/trajetoria_cidadao/src/main/java/com/smads/covs/trajetoria_cidadao/services/sisa_siscr_sisrua/DimTipoProcuraService.java
@@ -1,7 +1,10 @@
 package com.smads.covs.trajetoria_cidadao.services.sisa_siscr_sisrua;
 
+import com.smads.covs.trajetoria_cidadao.models.sisa_sicr_sisrua.DimTipoProcura;
 import com.smads.covs.trajetoria_cidadao.repositorys.sisa_siscr_sisrua.DimTipoProcuraRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 public class DimTipoProcuraService {
 
@@ -10,5 +13,9 @@ public class DimTipoProcuraService {
     @Autowired
     public DimTipoProcuraService(DimTipoProcuraRepository dimTipoProcuraRepository) {
         this.dimTipoProcuraRepository = dimTipoProcuraRepository;
+    }
+
+    public List<DimTipoProcura> findTipoProcura(Integer ciTipoProcura){
+        return dimTipoProcuraRepository.findAllByCiTipoProcura(ciTipoProcura);
     }
 }
