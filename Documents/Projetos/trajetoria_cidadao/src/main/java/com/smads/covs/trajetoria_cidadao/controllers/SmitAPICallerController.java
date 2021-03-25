c8cfb643b7c6e6f469d7a457380174830390b319
Pedro Sodre <pedssodre@gmail.com>
2021-03-25 11:54:53 -0300

    smitapicontroller instanciado

--- Documents/Projetos/trajetoria_cidadao/src/main/java/com/smads/covs/trajetoria_cidadao/controllers/SmitAPICallerController.java
+++ Documents/Projetos/trajetoria_cidadao/src/main/java/com/smads/covs/trajetoria_cidadao/controllers/SmitAPICallerController.java
@@ -1,6 +1,7 @@
 package com.smads.covs.trajetoria_cidadao.controllers;
 
 
+import com.smads.covs.trajetoria_cidadao.services.smit.SmitService;
 import org.springframework.stereotype.Controller;
 
 import java.math.BigInteger;
@@ -8,6 +9,12 @@ import java.util.List;
 
 @Controller
 public class SmitAPICallerController {
+
+    private final SmitService smitService;
+
+    public SmitAPICallerController(SmitService smitService) {
+        this.smitService = smitService;
+    }
 
     public List<?> getServicosData(BigInteger ciCidadao){
 
