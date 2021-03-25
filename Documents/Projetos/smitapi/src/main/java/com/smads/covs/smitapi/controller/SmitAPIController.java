4070fc3213346b39ad6997a1ae5964767984f28c
Victor de Souza Gomes Martins <x506821@rede.sp>
2021-03-25 16:44:25 -0300

    Commit inicial

---
+++ Documents/Projetos/smitapi/src/main/java/com/smads/covs/smitapi/controller/SmitAPIController.java
@@ -0,0 +1,20 @@
+package com.smads.covs.smitapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigInteger;
+import java.util.List;
+
+@RestController
+@RequestMapping("/cidadao")
+public class SmitAPIController {
+
+    @GetMapping("/smit/{ciCidadao}")
+    public String getServicosDados(@PathVariable("ciCidadao")BigInteger ciCidadao){
+
+        return "Deu certo";
+    }
+}
